26f69ce6cc0da9a97e161b61ffcc23a6f1bf7bb1
remi-sky <remi.chkaibane@gmail.com>
2025-07-03 11:46:50 +0100

    eslint fix

---
+++ front-end/.eslintrc.cjs
@@ -0,0 +1,27 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  plugins: ['react-hooks', 'jsx-a11y', 'react-refresh'],
+  extends: [
+    'eslint:recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+}
